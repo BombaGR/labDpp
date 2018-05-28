8766a09c29d5ceb29c05b0efd0f0570be809252c
Mateusz Wojcik <w.matiu@gmail.com>
2018-05-28 10:35:20 +0200

    init

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='vacationlibrery',
+      version='1.0',
+      author='Mateusz Wojcik',
+      author_email='226611@student.pwr.edu.pl',
+      url='https://github.com/BombaGR',
+      )
